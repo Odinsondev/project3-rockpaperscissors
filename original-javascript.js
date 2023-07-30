fdb8b13b24f83635ff95deb49dabada32df90b80
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2023-07-30 02:13:23 -0700

    Add buttons and edit DOM to display results

--- original-javascript.js
+++ original-javascript.js
@@ -1,4 +1,79 @@
-//Computer Choice
+//Step0 - test console
+
+console.log("Hello World");
+
+//step0.1 try to add variable inside a function
+
+let a = 5
+let b
+b = 3
+
+function simpleTest() {
+ return a + b;
+}
+
+console.log(simpleTest())
+
+
+
+function simpleTest2() {
+  let c = 2;
+  return a + c
+}
+ console.log(simpleTest2())
+
+console.log(a)
+console.log(b)
+//console.log(c)   - error as c is not in global scope
+
+
+
+//Step1 - figure out how to run and display result for math.random
+
+function getRandom() {
+  return Math.random();
+}
+
+console.log(getRandom())
+
+//Step2 - figure out how to get 1 or 2 or 3
+
+//Step2.1 - how to store result in a variable and use later
+
+function getRandom2(min, max) {
+  return Math.random();
+}
+
+let test = getRandom2()
+
+console.log(test)
+console.log(test)
+
+function add1(number) {
+  return test + number;
+}
+
+console.log(add1(1))
+
+//step2.2 - what  * (max - min) + min does
+
+function minmax(min, max) {
+  return test * (max - min) + min;
+}
+
+console.log(minmax(0.4, 0.5))
+
+//step2.3 - getting integers my way
+
+function minmax10(min, max) {
+  return (test * (max - min) + min)*10;
+}
+
+console.log(minmax10(0.4, 0.5))
+
+console.log(Math.floor(minmax10(0.4, 0.5)))
+
+//step2.4 - getting 1 or 2 or 3
 
 function getRandomInteger(min, max) {
   return Math.floor((Math.random() * (max - min) + min) * 10);
@@ -20,123 +95,44 @@ function getComputerChoice(choice) {
 
 console.log(getComputerChoice(choice))
 
-
-//Get player choice from button and get computer choice and playRound
-
-let playerSelection = ""
-let computerSelection = ""
-
-const buttonRock = document.getElementById('button1');
-const selection1 = buttonRock.addEventListener('click', selectRock);
-
-function selectRock() {
-  choice = getRandomInteger(0.1, 0.4)
-  console.log(choice)
-  computerSelection = getComputerChoice(choice);
-  console.log(getComputerChoice(choice))
-  playerSelection = "ROCK";
-  console.log(playRound(playerSelection, computerSelection));
-}
-
-const buttonPaper = document.getElementById('button2');
-const selection2 = buttonPaper.addEventListener('click', selectPaper);
-
-function selectPaper() {
-  choice = getRandomInteger(0.1, 0.4)
-  console.log(choice)
-  computerSelection = getComputerChoice(choice);
-  console.log(getComputerChoice(choice))
-  playerSelection = "PAPER";
-  console.log(playRound(playerSelection, computerSelection));
-}
-
-const buttonScissors = document.getElementById('button3');
-const selection3 = buttonScissors.addEventListener('click', selectScissors);
-
-function selectScissors() {
-  choice = getRandomInteger(0.1, 0.4)
-  console.log(choice)
-  computerSelection = getComputerChoice(choice);
-  console.log(getComputerChoice(choice))
-  playerSelection = "SCISSORS";
-  console.log(playRound(playerSelection, computerSelection));
-}
-
-
-//plays a single round of Rock Paper Scissors and modifies DOM to display Result
+//step3 - Write a function that plays a single round of Rock Paper Scissors
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection.toUpperCase() == "ROCK" && computerSelection.toUpperCase() == "ROCK") {
-    let div = document.getElementById('dir');
-    let p = document.getElementById('results');
-    results.textContent = "Tie. You both chose Rock.";
-    div.appendChild(p);
-    return "Tie. You both chose Rock.";
+    return "Tie. You both chose Rock."
   } else if (playerSelection.toUpperCase() == "ROCK" && computerSelection.toUpperCase() == "PAPER") {
-    let div = document.getElementById('dir');
-    let p = document.getElementById('results');
-    results.textContent = "Sorry! You loose. Paper beats rock.";
-    div.appendChild(p);
     return "Sorry! You loose. Paper beats rock."
   } else if (playerSelection.toUpperCase() == "ROCK" && computerSelection.toUpperCase() == "SCISSORS") {
-    let div = document.getElementById('dir');
-    let p = document.getElementById('results');
-    results.textContent = "Congratulations! You win. Rock beats scissors.";
-    div.appendChild(p);
     return "Congratulations! You win. Rock beats scissors."
   } else if (playerSelection.toUpperCase() == "PAPER" && computerSelection.toUpperCase() == "ROCK") {
-    let div = document.getElementById('dir');
-    let p = document.getElementById('results');
-    results.textContent = "Congratulations! You win. Paper beats rock.";
-    div.appendChild(p);
     return "Congratulations! You win. Paper beats rock."
   } else if (playerSelection.toUpperCase() == "PAPER" && computerSelection.toUpperCase() == "PAPER") {
-    let div = document.getElementById('dir');
-    let p = document.getElementById('results');
-    results.textContent = "Tie. You both chose paper.";
-    div.appendChild(p);
     return "Tie. You both chose paper."
   } else if (playerSelection.toUpperCase() == "PAPER" && computerSelection.toUpperCase() == "SCISSORS") {
-    let div = document.getElementById('dir');
-    let p = document.getElementById('results');
-    results.textContent = "Sorry! You loose. Scissors beat paper.";
-    div.appendChild(p);
     return "Sorry! You loose. Scissors beat paper."
   } else if (playerSelection.toUpperCase() == "SCISSORS" && computerSelection.toUpperCase() == "ROCK") {
-    let div = document.getElementById('dir');
-    let p = document.getElementById('results');
-    results.textContent = "Sorry! You loose. Rock beats scissors.";
-    div.appendChild(p);
     return "Sorry! You loose. Rock beats scissors."
   } else if (playerSelection.toUpperCase() == "SCISSORS" && computerSelection.toUpperCase() == "PAPER") {
-    let div = document.getElementById('dir');
-    let p = document.getElementById('results');
-    results.textContent = "Congratulations! You win. Scissors beats paper.";
-    div.appendChild(p);
     return "Congratulations! You win. Scissors beats paper."
   } else if (playerSelection.toUpperCase() == "SCISSORS" && computerSelection.toUpperCase() == "SCISSORS") {
-    let div = document.getElementById('dir');
-    let p = document.getElementById('results');
-    results.textContent = "Tie. You both chose scissors.";
-    div.appendChild(p);
     return "Tie. You both chose scissors."
   } else {
     return playerSelection + " is not a valid selection. Please try again!"
   }
 }
 
-//let playerChoice = prompt("'Rock, Paper or Scissors' Test round")
-//const playerSelection = playerChoice;
-//const computerSelection = getComputerChoice(choice);
+let playerChoice = prompt("'Rock, Paper or Scissors' Test round")
+const playerSelection = playerChoice;
+const computerSelection = getComputerChoice(choice);
 
 
-//console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection));
 
-//console.log("That was a little test. Now the real game starts. Best of 5. Good luck!")
+console.log("That was a little test. Now the real game starts. Best of 5. Good luck!")
 
 
 
-/* //5 rounds
+//5 rounds
 //round1
 
 function game(playerSelectionX, computerSelectionX) {
@@ -353,4 +349,3 @@ console.log(computerScore)
 //round6 - check winner
 
 console.log(checkWinner())
- */
